810df3beba436d39c29b8ec82426847bb6f3167f
Gianluca Guida <glguida@tlbflush.org>
2020-05-14 11:23:08 +0100

    Add envelope extractor

--- raw2plot.c
+++ raw2plot.c
@@ -10,15 +10,32 @@
 
 FILE *in, *out;
 
+static float env(float f)
+{
+  static float lmax = 0, pf = 0, diff = 0;
+  float pdiff;
+
+  pdiff = diff;
+  diff = f - pf;
+  if (pdiff >= 0 && diff < 0)
+    {
+      /* f > lmax or derivative changed sign into negative (zero case a *bit* edgy): local maximum. */
+      lmax = pf;
+    }
+  pf = f;
+  return lmax;
+}
+
 static void print(void)
 {
   unsigned long i;
   ssize_t r;
   float f;
 
+  fprintf(out, "sample out env\n");
   i = 0;
   while ((r = fread(&f, sizeof(float), 1, in)) == 1)
-    fprintf(out, "%ld %f\n", i++, f);
+    fprintf(out, "%ld %f %f\n", i++, f, env(f));
 }
 
 int main(int argc, char *argv[])
